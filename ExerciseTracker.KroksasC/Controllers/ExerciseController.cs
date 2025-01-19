15092d862f9c888628ab153c253aa9967be8846c
Gustas Čemrka <gustascemerka@gmail.com>
2025-01-19 23:28:04 +0200

    Add project files.

---
+++ ExerciseTracker.KroksasC/Controllers/ExerciseController.cs
@@ -0,0 +1,55 @@
+﻿using ExerciseTracker.KroksasC.Services;
+using Spectre.Console;
+
+namespace ExerciseTracker.KroksasC.Controllers
+{
+    internal class ExerciseController
+    {
+        private readonly ExerciseService _exerciseService;
+
+        // Constructor injection for ExerciseService
+        public ExerciseController(ExerciseService exerciseService)
+        {
+            _exerciseService = exerciseService;
+        }
+        public async Task Run()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to exercises logger!");
+
+                var option = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("What option do you want to choose?")
+                        .AddChoices("Create exercise", "View exercises", "Delete exercise", "Update exercise", "Exit")
+                );
+
+                switch (option)
+                {
+                    case "Create exercise":
+                        await _exerciseService.AddExercise();
+                        break;
+                    case "View exercises":
+                        await _exerciseService.ShowAllExercises();
+                        break;
+                    case "Delete exercise":
+                        await _exerciseService.DeleteExercise();
+                        break;
+                    case "Update exercise":
+                        await _exerciseService.UpdateExercise();
+                        break;
+                    case "Exit":
+                        Console.Clear();
+                        Console.WriteLine("Goodbye! Shutting down app..");
+                        Thread.Sleep(2000);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ExerciseTracker.KroksasC/Repositaries/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using ExerciseTracker.KroksasC.Models;
+
+namespace ExerciseTracker.KroksasC.Repositaries
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly ExerciseTrackerDbContext _context;
+        public IRepository<Exercise> Exercises { get; private set; }
+
+        public UnitOfWork(ExerciseTrackerDbContext context)
+        {
+            _context = context;
+            Exercises = new Repository<Exercise>(context);
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return -1;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
